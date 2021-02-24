36fc7cce8f99f0f4cb66fc90f4dad09595c1c5e1
HeidarSelman <heidar.selman@hotmail.com>
2021-02-24 15:25:07 +0100

    first commit

---
+++ src/test/java/heidar/booking/BookingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package heidar.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
